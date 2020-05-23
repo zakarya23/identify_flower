7c8b586384624449662d33f4afa7a4b86fe3d974
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2020-05-24 08:04:43 +1000

    Plotted all classes '

--- identify_flower.py
+++ identify_flower.py
@@ -1,8 +1,25 @@
 import sklearn 
-import PIL 
 import numpy as np 
 import argparse 
 import os 
+import pandas as pd 
+import copy 
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+columns = ["sepal length", "sepal width", "petal length", "petal width", "class"]
+data_set = pd.read_csv("iris_data.data", header = None, names = columns)
+
+new_dataset = data_set.select_dtypes(include = ['object']).copy() 
+
+class_count = new_dataset['class'].value_counts() 
+sb.set(style= "darkgrid")
+sb.barplot(class_count.index, class_count.values,alpha = 0.9)
+
+plt.title('Class Distribution')
+plt.ylabel('Number')
+plt.xlabel('Class')
+plt.show()
 
 
 def colour_stats(image): 
