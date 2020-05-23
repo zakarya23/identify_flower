459b5e7ddf95e758db263768e36b601469e19ceb
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2020-05-24 08:59:21 +1000

    Made neural network

--- identify_flower.py
+++ identify_flower.py
@@ -30,8 +30,15 @@ X = num_dataset[["sepal length", "sepal width", "petal length", "petal width"]]
 Y = num_dataset['Class']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y , test_size = 0.2)
 
-mlp = MLPClassifier(hidden_layer_sizes(10), solver = 'sgd', learning_rate_init = 0.01, max_iter= 500) 
+mlp = MLPClassifier(hidden_layer_sizes= 10, solver = 'sgd', learning_rate_init = 0.01, max_iter= 500) 
 
+mlp.fit(X_train, Y_train)
+
+MLPClassifier (activation = 'relu', alpha = 0.001, batch_size = 'auto', beta_1 = 0.9, beta_2 = 0.999, 
+               early_stopping = False, epsilon = 1e-08, hidden_layer_sizes = 10, learning_rate = 'constant', 
+               learning_rate_init = 0.01, max_iter = 500, momentum = 0.9, nesterovs_momentum = True, 
+               power_t = 0.5, random_state = None, shuffle = True, solver = 'sgd', tol = 0.001, 
+               validation_fraction = 0.1, verbose = False, warm_start = False)
 
 def colour_stats(image): 
     (R, G, B) = image.split() 
