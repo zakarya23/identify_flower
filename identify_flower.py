2427b2898277ce0d978f94d13c1d9ac32419360f
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2020-05-24 08:49:40 +1000

    Started Neural network

--- identify_flower.py
+++ identify_flower.py
@@ -1,25 +1,36 @@
 import sklearn 
 import numpy as np 
-import argparse 
-import os 
 import pandas as pd 
 import copy 
 import seaborn as sb
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
-columns = ["sepal length", "sepal width", "petal length", "petal width", "class"]
-data_set = pd.read_csv("iris_data.data", header = None, names = columns)
+columns = ["sepal length", "sepal width", "petal length", "petal width", "Class"]
+dataset = pd.read_csv("iris_data.data", header = None, names = columns)
 
-new_dataset = data_set.select_dtypes(include = ['object']).copy() 
+new_dataset = dataset.select_dtypes(include = ['object']).copy() 
 
-class_count = new_dataset['class'].value_counts() 
+class_count = new_dataset['Class'].value_counts() 
 sb.set(style= "darkgrid")
 sb.barplot(class_count.index, class_count.values,alpha = 0.9)
 
 plt.title('Class Distribution')
 plt.ylabel('Number')
 plt.xlabel('Class')
-plt.show()
+
+
+replace_classes = {'Class': {'Iris-setosa': 1, 'Iris-versicolor': 2, 'Iris-virginica':3}}
+
+num_dataset = dataset.copy() 
+num_dataset.replace(replace_classes, inplace = True)
+
+X = num_dataset[["sepal length", "sepal width", "petal length", "petal width"]]
+Y = num_dataset['Class']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y , test_size = 0.2)
+
+mlp = MLPClassifier(hidden_layer_sizes(10), solver = 'sgd', learning_rate_init = 0.01, max_iter= 500) 
 
 
 def colour_stats(image): 
